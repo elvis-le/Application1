47e79ccce0a6ef4738740a68dc135060fe313977
elvis-le <khanh321zx@gmail.com>
2023-12-04 12:18:37 +0700

    first commit

---
+++ FPTBook/wwwroot/js/SearchBar.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var searchNavbar = document.querySelector('.search_navbar');
+    var searchInputWrap = document.querySelector('.search-input-wrap');
+
+    searchNavbar.addEventListener('click', function (event) {
+        if (event.target.classList.contains('search-icon')) {
+            searchNavbar.classList.toggle('expanded');
+            if (searchNavbar.classList.contains('expanded')) {
+                searchInputWrap.querySelector('.search-input').focus();
+            }
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!searchNavbar.contains(event.target)) {
+            searchNavbar.classList.remove('expanded');
+        }
+    });
+});
